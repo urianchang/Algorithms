c354a3b0c7e09bafe8e3d1c8e7340b6ed4fa2350
Urian Chang <urianchang@gmail.com>
2017-02-17 08:56:44 -0800

    Completed Push Front algorithm.

---
+++ Chapter 3/pushfront.js
@@ -0,0 +1,19 @@
+/*
+Array: Push Front
+Given array and an additional value, insert this value at the beginning
+of the array. Do this without using any built-in array methods.
+*/
+
+function pushF(arr, value) {
+    arr[arr.length] = value;
+    for (var x = arr.length-1; x > 0; x--) {
+        temp = arr[x];
+        arr[x] = arr[x-1];
+        arr[x-1] = temp;
+    }
+    return arr;
+}
+
+arr = [1, 2, 3]
+value = 4
+console.log(pushF(arr, value));
