b71fb425f49579ebf5ea96d1be331411fb9f25c8
Urian Chang <urianchang@gmail.com>
2017-02-17 08:50:55 -0800

    Added the problem description to the algorithm.

--- Chapter 3/largest.js
+++ Chapter 3/largest.js
@@ -1,3 +1,11 @@
+/*
+Array: Nth-Largest
+
+Jimmy has "N" number of Green Belt stickers for excellent Python projects.
+Given arr and N, return the Nth-largest element, where (N-1) elements are
+larger. Return null if needed.
+*/
+
 function largest(arr, n){
     max = arr[0];
     min = arr[0];
