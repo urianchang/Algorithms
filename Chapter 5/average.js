a2f94e678b0963f843911e7d96d05eb503b8c83e
urian-mac <urianchang@gmail.com>
2017-03-05 17:34:00 -0800

    Completed SList: Average algorithm

---
+++ Chapter 5/average.js
@@ -0,0 +1,78 @@
+/*
+SList: Average
+
+Create average(node) to return the average value.
+*/
+
+//Node functions
+function ListNode(value) {
+  this.val = value;
+  this.next = null;
+  this.max = function () {
+    var max = this.val;
+    var N = this.next;
+    while (N) {
+      if (N.val > max) {
+        max = N.val;
+      }
+      N = N.next;
+    }
+    console.log("Maximum value: " + max);
+    return this;
+  }
+  this.min = function () {
+    var min = this.val;
+    var N = this.next;
+    while (N) {
+      if (N.val < min) {
+        min = N.val;
+      }
+      N = N.next;
+    }
+    console.log("Minimum value: " + min);
+    return this;
+  }
+  this.average = function () {
+    var sum = this.val;
+    var count = 1;
+    var N = this.next;
+    while (N) {
+      count++;
+      sum += N.val;
+      N = N.next;
+    }
+    console.log("Average: " + (sum/count));
+    return this;
+  }
+}
+
+function sLinkedList() {
+  this.head = null;
+  this.display = function () {
+    var N = this.head;
+    var string = "Node values in this list: ";
+    while (N.next) {
+      string += N.val + ", ";
+      N = N.next;
+    }
+    string += N.val
+    console.log(string);
+    return this;
+  }
+}
+
+//Create the list(s)
+var list1 = new sLinkedList();
+var node1 = new ListNode(6);
+var node2 = new ListNode(7);
+var node3 = new ListNode(8);
+list1.head = node1;
+node1.next = node2;
+node2.next = node3;
+
+var list2 = new sLinkedList();
+var node100 = new ListNode(100);
+list2.head = node100;
+
+node1.average();
+node100.average();
