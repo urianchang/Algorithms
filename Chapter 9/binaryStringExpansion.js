376556dfbae31bf23cb84b8bfbbad0a80818e8eb
urian-mac <urianchang@gmail.com>
2017-03-15 20:40:21 -0700

    Added comments to Binary String Expansion

--- Chapter 9/binaryStringExpansion.js
+++ Chapter 9/binaryStringExpansion.js
@@ -9,17 +9,25 @@ expanded to '0' or '1'.
 */
 
 function bse(string) {
-  var strArr = string.split("?", 2)
+  //: Split string by question mark.
+  var strArr = string.split("?")
+  //: If there are no question marks left, return the array of 1 string
   if (strArr.length == 1) {
     return strArr;
   }
+  //: Else, split the string into separate characters.
   strArr = string.split("");
+  //: Check every character
   for (var char = 0; char < strArr.length; char++) {
+      //: If question mark is found...
       if (strArr[char] == "?") {
+        //: Create a string with "?" replaced with "0"
         strArr[char] = 0;
         var string1 = strArr.join("");
+        //: Create another string with "?" replaced with "1"
         strArr[char] = 1;
         var string2 = strArr.join("");
+        //: Use recursive function and join/concatenate the string arrays
         return bse(string1).concat(bse(string2))
       }
   }
