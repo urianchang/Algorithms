1affa2b3e78dda1a517478ce5b3c3e9b9d3096e0
urian-mac <urianchang@gmail.com>
2017-03-17 18:31:13 -0700

    Scrap everything for String Anagrams. Rework it from beginning.

--- Chapter 9/stringAnagrams.js
+++ Chapter 9/stringAnagrams.js
@@ -5,16 +5,3 @@ Given a string, return array where each element is a string
 representing a different anagram. For this challenge,
 you can use built-in string functions such as split.
 */
-
-function anagram(string, arr) {
-  if (!arr) {arr = []};
-  if arr.indexOf(string) != -1 {
-    return arr;
-  }
-  var count = 0;
-  while (count < string.length) {
-    
-  }
-}
-
-console.log(anagram("test"));
