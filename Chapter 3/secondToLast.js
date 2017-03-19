e64b2e6b9647274a2f8c7aeb10680b8589aaa7d5
urian-mac <urianchang@gmail.com>
2017-03-18 23:11:01 -0700

    Completed Array: Second-To-Last algorithm

---
+++ Chapter 3/secondToLast.js
@@ -0,0 +1,17 @@
+/*
+Array: Second-to-Last
+
+Return the second-to-last element of an array.
+*/
+
+function secondLast(arr) {
+  if (arr.length < 1) {
+    return null;
+  }
+  return arr[arr.length-2];
+}
+
+var arr1 = [42, true, 4, "Liam", 7];
+var arr2 = [10];
+console.log(secondLast(arr1));
+console.log(secondLast(arr2));
