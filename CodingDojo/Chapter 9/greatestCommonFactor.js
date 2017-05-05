33115c1ded32ab3415fa43453eefd7bbbb4f510f
Urian Chang <urianchang@gmail.com>
2017-05-05 12:33:00 -0700

    Optimized the Greatest Common Factor algorithm to fulfill part II.

--- CodingDojo/Chapter 9/greatestCommonFactor.js
+++ CodingDojo/Chapter 9/greatestCommonFactor.js
@@ -28,6 +28,26 @@ function rGCF(param1, param2) {
 
 console.log(rGCF(3, 7));    // 1
 console.log(rGCF(18, 27));  // 9
-console.log(rGCF(49, 42));  // 7
+console.log(rGCF(49, 14));  // 7
 
 //: Part II
+function GCF(param1, param2) {
+    if (param1 == param2) {
+        return param1;
+    }
+    if (param1 > param2) {
+        if ((param1/param2) % 1 === 0) {
+            return param2;
+        } else {
+            return GCF(param1-param2, param2);
+        }
+    }
+    if (param1 < param2) {
+        if ((param1/param2) % 1 === 0) {
+            return param2;
+        } else {
+            return GCF(param1, param2-param1);
+        }
+    }
+}
+console.log(GCF(123456, 987654));   // 6
