0553c774cd7db2b35eae2f91c0d1f7d89410003d
Urian Chang <urianchang@gmail.com>
2017-05-06 23:40:06 -0700

    Started working on the string to word array algorithm

---
+++ CodingDojo/Chapter 10/stringToWordArray.js
@@ -0,0 +1,21 @@
+/*
+String to Word Array
+
+Given a string of words (with spaces,
+tabs, and linefeeds), return an array
+of words.
+
+Bonus: handle punctuation
+*/
+
+function wordArr(str) {
+    var arr = [];
+    var wordStr = "";
+    for (var i=0; i < str.length; i++) {
+        if (str[i] == )
+    }
+    return arr;
+}
+
+var str1 = "Life is not a drill!";
+console.log(wordArr(str1));
