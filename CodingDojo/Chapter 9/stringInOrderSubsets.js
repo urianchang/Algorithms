b6c109b08285e4d079356bd2fece233ee69a7ac1
Urian Chang <urianchang@gmail.com>
2017-05-10 12:49:00 -0700

    Added comments to Recursive String In Order Subsets algorithm

--- CodingDojo/Chapter 9/stringInOrderSubsets.js
+++ CodingDojo/Chapter 9/stringInOrderSubsets.js
@@ -12,31 +12,42 @@ function strSubsets(str) {
     return makeSubsets(str, [], str.length - 1);
 }
 
+//: Helper function that uses recursion and memoization to make array of subsets
 function makeSubsets(str, arr, memo) {
+    //: Include an empty string subset
     if (memo == str.length - 1) {
         arr.push("");
     }
+    //: Break case - When memo (index) is less than zero
     if (memo < 0) {
-        // arr.push(str);
         return arr;
     }
+    //: Get the specific character at memo (index)
     var char = str[memo];
     arr.push(char);
+
+    //: Using two for-loops to generate string subsets since string slice is used
+        //: First for-loop determines starting point of string slice
     for (var i = 0; i < memo; i++) {
-        for (var idx = i+1; idx < str.length; idx++){
-            // console.log(i, idx, str.slice(i, idx));
+        //: Second for-loop determines ending point of string slice
+        for (var idx = i+1; idx < str.length; idx++) {
+            //: If starting and ending slice indices are the same, continue
             if (idx == i) {
                 continue;
+            //: If ending slice index is less than memo, add the substring to array
             } else if (idx <= memo) {
                 arr.push(str.slice(i, idx) + char);
             } else {
+                //: If ending slice index is greater than memo...
                 var newSub = str.slice(i, memo-1) + char + str.slice(idx);
+                //: Check for duplicates...
                 if (arr.includes(newSub) === false) {
                     arr.push(newSub);
                 }
             }
         }
     }
+    //: Recurisve call - Return function with memo reduced by 1
     return makeSubsets(str, arr, memo - 1);
 }
 
@@ -44,4 +55,4 @@ var str1 = "abc";
 console.log(strSubsets(str1));   // ["", "c", "b", "bc", "a", "ac", "ab", "abc"] in any order
 
 var str2 = "abcd";
-console.log(strSubsets(str2));
+console.log(strSubsets(str2).length);
