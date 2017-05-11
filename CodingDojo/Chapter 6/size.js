e4dd77c052174d1144a9710f88a389a145d47edd
Urian Chang <urianchang@gmail.com>
2017-05-11 11:01:28 -0700

    Completed SLQueue: Size algorithm

--- CodingDojo/Chapter 6/size.js
+++ CodingDojo/Chapter 6/size.js
@@ -6,4 +6,30 @@ number of values in our queue.
 */
 
 const Node = require('./_NodeSLL');
-const Q = require('./_SLQueue');
+
+function SLQueue() {
+    var head = null;
+    var tail = null;
+}
+
+SLQueue.prototype.size = function() {
+    if (!this.head) {
+        return 0;
+    }
+    var count = 0;
+    var runner = this.head;
+    while (runner) {
+        count++;
+        runner = runner.next;
+    }
+    return count;
+}
+
+var q = new SLQueue();
+var node1 = new Node(1);
+var node2 = new Node(2);
+console.log(`Size of queue: ${q.size()}`);
+q.head = node1;
+console.log(`Size of queue: ${q.size()}`);
+node1.next = node2;
+console.log(`Size of queue: ${q.size()}`);
