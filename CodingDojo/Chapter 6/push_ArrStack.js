8136cdba98f160371b614c37b53936a100735a2d
Urian Chang <urianchang@gmail.com>
2017-05-12 11:00:32 -0700

    Completed ArrStack: Push algorithm

--- CodingDojo/Chapter 6/push_ArrStack.js
+++ CodingDojo/Chapter 6/push_ArrStack.js
@@ -1,5 +1,18 @@
 /*
 ArrStack: Push
 
-
+Create push(val) that adds val to our stack.
 */
+
+function ArrStack() {
+    this.values = [];
+}
+
+ArrStack.prototype.push = function(val) {
+    this.values.push(val);
+    console.log(`${val} has been added to the stack`);
+    return this;
+}
+
+var stack = new ArrStack();
+stack.push(1).push(2);
