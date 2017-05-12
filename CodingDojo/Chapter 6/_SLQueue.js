f6d659b0210ee4c5817653412105dfd4463052f5
Urian Chang <urianchang@gmail.com>
2017-05-12 11:05:00 -0700

    Modified SLQueue methods to return self when possible.

--- CodingDojo/Chapter 6/_SLQueue.js
+++ CodingDojo/Chapter 6/_SLQueue.js
@@ -17,32 +17,40 @@ SLQueue.prototype.enqueue = function(val) {
         }
         runner.next = newNode;
     }
+    console.log(`${val} has been added to the queue`);
+    return this;
 }
 
 //: Dequeue / Remove and return the value at the front of the queue
 SLQueue.prototype.dequeue = function() {
     if (!this.head) {
-        return "Queue is empty";
+        console.log("Queue is empty");
+        return this;
     }
     var removedNode = this.head;
     this.head = this.head.next;
-    return `Dequeued value: ${removedNode.val}`;
+    console.log(`Dequeued value: ${removedNode.val}`);
+    return removedNode.val;
 }
 
 //: Return the value at the front of the queue
 SLQueue.prototype.front = function() {
     if (!this.head) {
-        return "Queue is empty";
+        console.log("Queue is empty");
+        return null;
     }
-    return `Value at front of queue: ${this.head.val}`;
+    console.log(`Value at front of queue: ${this.head.val}`);
+    return this.head.val;
 }
 
 //: Check if queue is empty
 SLQueue.prototype.isEmpty = function() {
     if (!this.head) {
-        return "Queue is empty";
+        console.log("Queue is empty");
+        return this;
     }
-    return "Queue is NOT empty";
+    console.log("Queue is NOT empty");
+    return this;
 }
 
 //: Return the size of the queue
@@ -62,16 +70,19 @@ SLQueue.prototype.size = function() {
 //: Check if queue contains a given value
 SLQueue.prototype.contains = function(val) {
     if (!this.head) {
-        return "Queue is empty";
+        console.log("Queue is empty");
+        return this;
     }
     var runner = this.head;
     while (runner) {
         if (runner.val === val) {
-            return `${val} is in the queue`;
+            console.log(`${val} is in the queue`);
+            return this;
         }
         runner = runner.next;
     }
-    return `${val} is NOT in the queue`;
+    console.log(`${val} is NOT in the queue`);
+    return this;
 }
 
 module.exports = SLQueue;
