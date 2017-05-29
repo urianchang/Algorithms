c672d952c3e5d2a5aa6562b42d58d67d92336ba3
Urian Chang <urianchang@gmail.com>
2017-05-29 00:26:06 -0700

    Completed bonus for Reverse Word Order

--- CodingDojo/Chapter 10/reverseWordOrder.js
+++ CodingDojo/Chapter 10/reverseWordOrder.js
@@ -4,6 +4,8 @@ Reverse Word Order:
 Create a function that, given a string of
 words (with spaces), returns new string
 with words in reverse sequence.
+
+BONUS: handle punctuation and capitalization.
 */
 
 function reverseWordOrder(str) {
@@ -16,7 +18,45 @@ function reverseWordOrder(str) {
     return wordArr.join(" ");
 }
 
-var str1 = "This is a test";
-console.log(reverseWordOrder(str1));
-var str2 = "This is not a test for you";
-console.log(reverseWordOrder(str2));
+// var str1 = "This is a test";
+// console.log(reverseWordOrder(str1));
+// var str2 = "This is not a test for you";
+// console.log(reverseWordOrder(str2));
+
+var str1 = "Hello world, i am here; right?";
+
+//: For the bonus...
+function revWordOrder(str) {
+    var newArr = [];
+    var endingPunctuation = "";
+    //: Split string into words
+    var wordArr = str.split(" ");
+    //: Iterate through array backwards
+    for (var i = wordArr.length - 1; i >= 0; i--) {
+        var word = "";
+        //: Split every word into characters
+        var charArr = wordArr[i].split("");
+        //: Iterate through characters
+        for (var idx = 0; idx < charArr.length; idx++) {
+            if (i === wordArr.length - 1 && idx === 0) {
+                charArr[idx] = charArr[idx].toUpperCase();
+            }
+            if (i === 0 && idx === 0) {
+                charArr[idx] = charArr[idx].toLowerCase();
+            }
+            if (charArr[idx] === "," || charArr[idx] === ";" || charArr[idx] === "'" || charArr[idx] === ":" || charArr[idx] === '"') {
+                newArr[(wordArr.length - 1) - (i + 1)] = newArr[(wordArr.length - 1) - (i + 1)] + charArr[idx];
+                continue;
+            }
+            if (charArr[idx] === "." || charArr[idx] === "!" || charArr[idx] === "?") {
+                endingPunctuation = charArr[idx];
+                continue;
+            }
+            word += charArr[idx];
+        }
+        newArr.push(word);
+    }
+    newArr[newArr.length - 1] = newArr[newArr.length - 1] + endingPunctuation;
+    return newArr.join(" ");
+}
+console.log(revWordOrder(str1));
