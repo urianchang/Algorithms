ea09a4f651f4c1c8676aac835e0491636957af78
Urian Chang <urianchang@gmail.com>
2017-06-01 12:04:50 -0700

    Started HackerRank algorithms in Python. Completed Day 1 of 30 day challenge.

---
+++ HackerRank/30_Days_of_Code/day1_dataTypes.py
@@ -0,0 +1,22 @@
+"""
+Day 1: Data Types
+
+1. Declare 3 variables: int, double, String
+2. Read 3 lines of input from stdin and initialize 3 variables
+3. Use '+' operator to perform the following operations:
+    1. print sum of i plus int
+    2. print sum of d plus double variable
+    3. concatenate s with string you read as input and print it
+"""
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+integer = int(raw_input())
+double = float(raw_input())
+string = raw_input()
+
+print integer + i
+print double + d
+print s + string
