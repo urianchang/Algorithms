1284aaa2ee4cade64493d5cc93b732793729d6ab
Urian Chang <urianchang@gmail.com>
2017-06-16 12:22:56 -0700

    Tried the Transform to Palindrome algorithm. Runtime error in solution.

--- HackerRank/Week_Of_Code_33/transformPalindrome.py
+++ HackerRank/Week_Of_Code_33/transformPalindrome.py
@@ -1,4 +1,3 @@
-
 """
 Transform to Palindrome:
 
@@ -13,6 +12,8 @@ Print the length of the longest possible palindromic subsequence
 after applying zero or more transformations on the letters of the
 given sequence.
 """
+
+import sys
 
 n,k,m = raw_input().strip().split(' ')
 n,k,m = [int(n),int(k),int(m)]
@@ -39,25 +40,36 @@ for a0 in xrange(k):
 
 a = map(int, raw_input().strip().split(' '))
 
-for i in range(len(a)):
-    while a[i] in rules:
-        a[i] = rules[a[i]]
+# Function used to find longest palindrome using Dynamic Programming
+def lps(arr, rules):
+    n = len(arr)
 
-maxCount = 1
-for i in range(len(a)):
-    count = 0
-    start = i
-    end = len(a) - 1
-    while start < end:
-        if a[start] == a[end]:
-            count += 2
-            start += 1
-            end -= 1
-        else:
-            if end - start == 1:
-                count += 1
-            end -= 1
-    if count > maxCount:
-        maxCount = count
+    # Create a 2D table to store results: initialize with '0'
+    L = [[0 for x in range(n)] for x in range(n)]
 
-print maxCount
+    # Strings of length 1 are palindrome of length 1
+    for i in range(n):
+        L[i][i] = 1
+
+    # Create subsequences
+    # l is length of substring
+    for l in range(2, n+1):
+        for i in range(n-l+1):
+            j = i+l-1
+            while arr[i] in rules:
+                arr[i] = rules[arr[i]]
+            while arr[j] in rules:
+                arr[j] = rules[arr[j]]
+            if arr[i] == arr[j] and l == 2:
+                L[i][j] = 2
+            elif arr[i] == arr[j]:
+                L[i][j] = L[i+1][j-1] + 2
+            else:
+                L[i][j] = max(L[i][j-1], L[i+1][j])
+
+    return L[0][n-1]
+
+# for i in range(len(a)):
+#     while a[i] in rules:
+#         a[i] = rules[a[i]]
+print lps(a, rules)
