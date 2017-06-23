1c24f8658374fdf85fabccd6fba5a3bc647b2701
Urian Chang <urianchang@gmail.com>
2017-06-23 00:25:12 -0700

    Completed Zip it algorithm

--- CodingDojo/Chapter 3/zipIt.js
+++ CodingDojo/Chapter 3/zipIt.js
@@ -1,4 +1,26 @@
-*/
+/*
 Zip It
 
+Create a standalone function that accepts two arrays
+and combines their values sequentially into a new
+array, at alternating indices starting with first array.
+Extra values from either array should be included afterward.
 */
+
+function zip(a1, a2) {
+    var newArr = [];
+    var count = (a1.length > a2.length ? a1.length : a2.length);
+    for (var i = 0; i < count; i++) {
+        if ( i < a1.length ) {
+            newArr.push(a1[i]);
+        }
+        if ( i < a2.length ) {
+            newArr.push(a2[i]);
+        }
+    }
+    return newArr;
+}
+
+var arr1 = [1, 2];
+var arr2 = [10, 20, 30, 40];
+console.log(zip(arr1, arr2)); //: [1, 10, 2, 20, 30, 40]
