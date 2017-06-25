b1f69ed83747d71b4659b705fac809e61392dc18
Urian Chang <urianchang@gmail.com>
2017-06-25 00:23:32 -0700

    Completed BST: Contains algorithm

--- CodingDojo/Chapter 11/contains.js
+++ CodingDojo/Chapter 11/contains.js
@@ -18,6 +18,72 @@ function BST() {
     this.root = null;
 }
 
-BST.prototype.contains = function(value) {
-
+//: Helper Add function to create tree
+BST.prototype.add = function(value) {
+    //: Check if root node exists
+    if (this.root === null) {
+        this.root = new BTNode(value);
+    } else {
+        //: Compare value at each node and move accordingly
+        var runner = this.root;
+        while (runner) {
+            //: If value is equal...
+            if (value === runner.val) {
+                runner.count++;
+                break;
+            //: If value is less than...
+            } else if (value < runner.val) {
+                if (!runner.left) {
+                    runner.left = new BTNode(value);
+                    break;
+                } else {
+                    runner = runner.left;
+                }
+            //: If value is greater than...
+            } else {
+                if (!runner.right) {
+                    runner.right = new BTNode(value);
+                    break;
+                } else {
+                    runner = runner.right;
+                }
+            }
+        }
+    }
 }
+
+BST.prototype.contains = function(value) {
+    //: Check if root node exists
+    if (this.root === null) {
+        return false;
+    }
+    var runner = this.root;
+    while (runner) {
+        if (value === runner.val) {
+            return true;
+        }
+        if (value < runner.val) {
+            if (!runner.left) {
+                return false;
+            }
+            runner = runner.left;
+        } else {
+            if (!runner.right) {
+                return false;
+            }
+            runner = runner.right;
+        }
+    }
+    return false;
+}
+
+var tree = new BST();
+console.log(tree.contains(4));  //: false
+tree.add(3);
+console.log(tree.contains(4));  //: false
+tree.add(4);
+console.log(tree.contains(4));  //: true
+tree.add(2);
+console.log(tree.contains(1));  //: false
+tree.add(1);
+console.log(tree.contains(1));  //: true
