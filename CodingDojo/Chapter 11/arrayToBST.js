73809e31e51d9be423e9f766b96ae52bba457e3d
Urian Chang <urianchang@gmail.com>
2017-07-03 22:51:31 -0700

    Completed array to BST algorithm

--- CodingDojo/Chapter 11/arrayToBST.js
+++ CodingDojo/Chapter 11/arrayToBST.js
@@ -11,15 +11,32 @@ const BTNode = require('./__BSTnode');
 //: Require BST object
 let BST = require('./__BST');
 
-var tree = new BST();
-console.log(tree.isBalanced()); //: true
-tree.add(3);
-tree.add(2);
-tree.add(1);
-console.log(tree.isBalanced()); //: false
-tree.add(4);
-console.log(tree.isBalanced()); //: true
-tree.add(5);
-tree.add(6);
-tree.add(7);
-console.log(tree.isBalanced()); //: false
+function array2bst(arr) {
+    let tree = new BST();
+    let newArr = reorder(arr, []);
+    for (var i = 0; i < newArr.length; i++) {
+        tree.add(newArr[i]);
+    }
+    return tree;
+}
+
+function reorder(arr, memo) {
+    if (arr.length === 0) {
+        return;
+    }
+    let mid = Math.floor(arr.length/2);
+    memo.push(arr[mid]);
+    //: Recursively add mid points in the bottom half
+    reorder(arr.slice(0, mid), memo);
+    //: Recursively add mid points in the top half
+    reorder(arr.slice(mid+1, arr.length), memo);
+    return memo;
+}
+
+let arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let tree1 = array2bst(arr1);
+console.log(tree1.isBalanced());    //: true
+
+let arr2 = [2, 4, 6, 8];
+let tree2 = array2bst(arr2);
+console.log(tree2.isBalanced());    //: true
