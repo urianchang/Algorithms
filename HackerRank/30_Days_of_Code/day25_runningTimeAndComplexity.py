99dd0b72def9eaf1c17eea330a737106774deb17
Urian Chang <urianchang@gmail.com>
2017-07-07 23:14:47 -0700

    Completed Day 25: Running Time and Complexity

--- HackerRank/30_Days_of_Code/day25_runningTimeAndComplexity.py
+++ HackerRank/30_Days_of_Code/day25_runningTimeAndComplexity.py
@@ -6,3 +6,24 @@ that has no positive divisors other than 1
 and it itself. Given a number, 'n', determine
 and print whether it's Prime or Not prime.
 """
+
+def primality(n):
+    if n == 0 or n == 1:
+        return "Not prime"
+    if n == 2 or n == 3:
+        return "Prime"
+    if n%2 == 0:
+        return "Not prime"
+    else:
+        i = 3
+        while i*i <= n:
+            if n%i == 0:
+                return "Not prime"
+            i += 2
+        return "Prime"
+
+T = int(raw_input().strip())
+
+for i in range(T):
+    num = int(raw_input())
+    print primality(num)
