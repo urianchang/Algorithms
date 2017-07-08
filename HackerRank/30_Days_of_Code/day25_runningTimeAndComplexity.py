dbf0a4405b141e732fe319ddf3b9a34885582db7
Urian Chang <urianchang@gmail.com>
2017-07-07 21:26:45 -0700

    Started working on Day 25: Running Time and Complexity

---
+++ HackerRank/30_Days_of_Code/day25_runningTimeAndComplexity.py
@@ -0,0 +1,8 @@
+"""
+Day 25: Running Time and Complexity
+
+A prime is a natural number greater than 1
+that has no positive divisors other than 1
+and it itself. Given a number, 'n', determine
+and print whether it's Prime or Not prime.
+"""
