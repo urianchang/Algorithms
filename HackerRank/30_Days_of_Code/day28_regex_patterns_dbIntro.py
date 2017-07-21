65f75e46c89481ee979ee5f3804e4b7f4e216866
Urian Chang <urianchang@gmail.com>
2017-07-20 23:41:58 -0700

    Added day 28: regex

---
+++ HackerRank/30_Days_of_Code/day28_regex_patterns_dbIntro.py
@@ -0,0 +1,9 @@
+"""
+Day 28: RegEx, Patterns, and Intro to Databases
+
+Consider a database table, 'Emails', which has the
+attributes, 'First Name' and 'Email ID'. Given rows
+of data simulating the 'Emails' table, print an
+alphabetically-ordered list of people whose email
+address ends in @gmail.com.
+"""
