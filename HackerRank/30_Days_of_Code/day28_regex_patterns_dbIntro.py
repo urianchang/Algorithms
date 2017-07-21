7ed5efcc154327a64bc23e58b74b821123aa34f3
Urian Chang <urianchang@gmail.com>
2017-07-21 00:05:29 -0700

    Completed day 28: regex

--- HackerRank/30_Days_of_Code/day28_regex_patterns_dbIntro.py
+++ HackerRank/30_Days_of_Code/day28_regex_patterns_dbIntro.py
@@ -9,11 +9,18 @@ address ends in @gmail.com.
 """
 
 import sys
+import re # For regex
 
 N = int(raw_input().strip())
+
+solutions = []
 
 for a0 in xrange(N):
     firstName, emailID = raw_input().strip().split(' ')
     firstName, emailID = [str(firstName),str(emailID)]
-    print firstName
-    print emailID
+    prog = re.compile('.*@gmail.com$')
+    if prog.match(emailID) is not None:
+        solutions.append(firstName)
+
+for ch in sorted(solutions):
+    print ch
