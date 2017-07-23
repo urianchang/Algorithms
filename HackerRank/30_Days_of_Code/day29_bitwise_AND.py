8cabd34eb8a233ea21d57dc11cbac7f95cea1f3d
Urian Chang <urianchang@gmail.com>
2017-07-22 23:53:45 -0700

    Completed Day 29: Bitwise AND

--- HackerRank/30_Days_of_Code/day29_bitwise_AND.py
+++ HackerRank/30_Days_of_Code/day29_bitwise_AND.py
@@ -9,8 +9,11 @@ bitwise AND operator.
 
 import sys
 
-
 t = int(raw_input().strip())
 for a0 in xrange(t):
     n,k = raw_input().strip().split(' ')
     n,k = [int(n),int(k)]
+    if (k-1) | k <= n:
+        print k-1
+    else:
+        print k-2
