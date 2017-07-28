1fb6b46d96b5d7fdbd89c1207bbd3fb750ef647e
Urian <urianchang@gmail.com>
2017-07-27 22:51:04 -0700

    Completed Day 27: Testing... really dumb.

--- HackerRank/30_Days_of_Code/day27_testing.py
+++ HackerRank/30_Days_of_Code/day27_testing.py
@@ -3,3 +3,15 @@ Day 27: Testing
 
 Create and print a test case for the problem that meets the criteria.
 """
+
+print 5
+print 5, 3
+print -1, 90, 999, 100, 0
+print 4, 2
+print 0, -1, 2, 1
+print 3, 3
+print -1, 0, 1
+print 6, 1
+print -1, 0, 1, -1, 2, 3
+print 7, 3
+print -1, 0, 1, 2, 3, 4, 5
