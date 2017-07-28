1d751033bf01131ebf53a5c754785ca0769d36e0
Urian <urianchang@gmail.com>
2017-07-28 11:51:24 -0700

    Completed Left Rotation algorithm

--- HackerRank/Algorithms/DataStructures/Arrays/leftRotation.py
+++ HackerRank/Algorithms/DataStructures/Arrays/leftRotation.py
@@ -6,10 +6,12 @@ of the array's elements 1 unit to the left. For example,
 if 2 left rotations are performed on array [1, 2, 3, 4, 5],
 then the array would become [3, 4, 5, 1, 2].
 
-Given an array of 'n' integers and a number, 'd', perform 'd'
+Given an array of 'n' integers and a number 'd', perform 'd'
 left rotations on the array. Then print the updated array as
 a single line of space-separated integers.
 """
 
 n, d = map(int, raw_input().strip().split())
 arr = map(int,raw_input().strip().split(' '))
+arr = arr[d:] + arr[0:d]
+print ' '.join(map(str, arr))
