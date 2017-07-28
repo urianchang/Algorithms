e638248bf9e3ab24bf9f3317fc03a0a5cdd85d2c
Urian <urianchang@gmail.com>
2017-07-28 11:54:28 -0700

    Started working on Sparse Arrays algorithm

---
+++ HackerRank/Algorithms/DataStructures/Arrays/sparseArrays.py
@@ -0,0 +1,9 @@
+"""
+Sparse Arrays
+
+There is a collection of 'N' strings. Each string's length
+is no more than 20 characters. There are also 'Q' queries.
+For each query, you are given a string, and you need to find
+out how many times this string occurs in the given collection
+of 'N' strings.
+"""
