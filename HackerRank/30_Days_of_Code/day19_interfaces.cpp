cf01577cda2fb4b7c946c049e7af083abfdb0f26
Urian <urianchang@gmail.com>
2017-07-27 22:54:14 -0700

    Started Day 19: Interfaces. Need to use C++

---
+++ HackerRank/30_Days_of_Code/day19_interfaces.cpp
@@ -0,0 +1,22 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+class AdvancedArithmetic{
+    public:
+        virtual int divisorSum(int n)=0;
+};
+
+
+
+int main(){
+    int n;
+    cin >> n;
+    AdvancedArithmetic *myCalculator = new Calculator();
+    int sum = myCalculator->divisorSum(n);
+    cout << "I implemented: AdvancedArithmetic\n" << sum;
+    return 0;
+}
