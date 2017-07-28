003c8472aa6643bb6e65bd7ccee8d82523c9356d
Urian <urianchang@gmail.com>
2017-07-27 23:09:14 -0700

    Completed Day 19 with help

--- HackerRank/30_Days_of_Code/day19_interfaces.cpp
+++ HackerRank/30_Days_of_Code/day19_interfaces.cpp
@@ -1,3 +1,14 @@
+/*
+Day 19: Interfaces
+
+The AdvancedArithmetic interface and the method declaration
+for the abstract int divisorSum(int n) method are provided
+for you in the editor below. Write the Calculator class, which
+implements the AdvancedArithmetic interface. The implementation
+for the divisorSum method must be public and take an integer parameter
+'n' and return the sum of all its divisors. 
+*/
+
 #include <cmath>
 #include <cstdio>
 #include <vector>
@@ -10,7 +21,16 @@ class AdvancedArithmetic{
         virtual int divisorSum(int n)=0;
 };
 
-
+class Calculator : public AdvancedArithmetic{
+    public:
+        virtual int divisorSum(int n) {
+            int sum = 0;
+            for(int i = 1; i <= n; i++) {
+                if(n % i == 0) { sum += i; }
+            }
+            return sum;
+        }
+};
 
 int main(){
     int n;
