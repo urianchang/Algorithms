83da1727c308329bd752c1a1408bf90153247b88
Urian <urianchang@gmail.com>
2017-07-30 01:35:11 -0700

    Started CamelCase algorithm

---
+++ HackerRank/Algorithms/Strings/camelCase.py
@@ -0,0 +1,16 @@
+"""
+Camel Case:
+
+Alice wrote a sequence of words in CamelCase as a string of letters 's',
+having the properties:
+    * It's a concatenation of one or more words consisting of English letters
+    * All letters in the first word are lowercase
+    * For each of the subsequent words, the first letter is uppercase
+        and rest of the letters are lowercase
+
+Given 's', print the number of words in s on a new line.
+"""
+
+import sys
+
+s = raw_input().strip()
