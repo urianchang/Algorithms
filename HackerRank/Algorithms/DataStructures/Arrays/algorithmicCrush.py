d0b17602cce4e53f5723cc257476c477152b103e
Urian <urianchang@gmail.com>
2017-08-10 10:59:30 -0700

    Completed Algorithmic Crush with 2 solutions: One shown is optimized to run quicker than the one that's commented out.

--- HackerRank/Algorithms/DataStructures/Arrays/algorithmicCrush.py
+++ HackerRank/Algorithms/DataStructures/Arrays/algorithmicCrush.py
@@ -18,3 +18,37 @@ Sample input:
 Output:
 200
 """
+
+N, M = map(int, raw_input().strip().split(' '))
+
+l = [0] * (N+1)
+
+for _ in range(M):
+    a, b, k = map(int, raw_input().strip().split(' '))
+    # Only store the difference
+    l[a-1] += k
+    if b <= len(l):
+        l[b] -= k
+
+maximum = x = 0
+# Iterate through the list, adding up the values to find the max 
+for i in l:
+    x = x + i
+    maximum = max(x, maximum)
+
+print maximum
+
+'''
+This solution times out after Test Case #6...
+
+maximum = 0
+
+for _ in xrange(M):
+    a, b, k = map(int, raw_input().strip().split(' '))
+    for num in range(a, b+1):
+        l[num] += k
+        if l[num] > maximum:
+            maximum = l[num]
+
+print maximum
+'''
