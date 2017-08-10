99c959fcb64359f8ddb09a2a88b38da29b1c6214
Urian <urianchang@gmail.com>
2017-08-09 23:48:26 -0700

    Started working on Algorithmic Crush algorithm

---
+++ HackerRank/Algorithms/DataStructures/Arrays/algorithmicCrush.py
@@ -0,0 +1,20 @@
+'''
+Algorithmic Crush:
+
+You are given a list of size 'N', initialized with zeroes. You have to perform
+'M' operations on the list and output the maximum of final values of all the
+'N' elements in the list. For every operation, you are given three integers,
+'a', 'b', and 'k'. You have to add value k to all the elements ranging from index
+a to b (both inclusive).
+'''
+
+"""
+Sample input:
+5 3
+1 2 100
+2 5 100
+3 4 100
+
+Output:
+200
+"""
