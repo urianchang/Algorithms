ff0a9c25719b323bb35df7b911058447b7ee35ea
Urian Chang <urianchang@gmail.com>
2017-08-13 22:06:32 -0700

    Completed Insert Node at N algorithm. The problem itself is not defined clearly.

--- HackerRank/Algorithms/DataStructures/LinkedLists/insertNodeAtN.py
+++ HackerRank/Algorithms/DataStructures/LinkedLists/insertNodeAtN.py
@@ -10,13 +10,15 @@ class Node(object):
         self.next = next_node
 
 def InsertNth(head, data, position):
-    if head == None:
-        head = Node(data)
+    if head is None:
+        head = Node(data, None)
+    elif position == 0:
+        head = Node(data, head)
     else:
         node = Node(data)
         runner = head
         for _ in range(position - 1):
             runner = runner.next
-        node.next = runner.next.next
+        node.next = runner.next
         runner.next = node
     return head
