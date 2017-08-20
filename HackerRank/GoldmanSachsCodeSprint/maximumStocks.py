4e24459dc03b2d07abe2237a7b755f4989d44fb9
Urian <urianchang@gmail.com>
2017-08-20 12:13:52 -0700

    Completed Maximum Stocks algorithm: 23.21

--- HackerRank/GoldmanSachsCodeSprint/maximumStocks.py
+++ HackerRank/GoldmanSachsCodeSprint/maximumStocks.py
@@ -12,7 +12,45 @@ stocks they can buy.
 import sys
 
 def buyMaximumProducts(n, k, a):
-    
+    stocks = 0
+    d = {}
+    for idx in xrange(len(a)):
+        if a[idx] in d:
+            d[a[idx]].append(idx + 1)
+        else:
+            d[a[idx]] = [idx + 1]
+    avail = sorted(d.keys())
+    for i in xrange(len(avail)):
+        if k <= 0 or k <= avail[i]:
+            return stocks
+        else:
+            while d[avail[i]]:
+                d[avail[i]].sort()
+                maxQuantity = d[avail[i]].pop()
+                while (maxQuantity > 0):
+                    cost = maxQuantity * avail[i]
+                    if k > cost:
+                        k -= cost
+                        stocks += maxQuantity
+                        break
+                    else:
+                        maxQuantity -= 1
+    return stocks
+
+    # a.sort()
+    # for i in xrange(len(a)):
+    #     if k <= 0 or k <= a[i]:
+    #         return stocks
+    #     maxQuantity = d[a[i]].pop(d[a[i]].index(max(d[a[i]])))
+    #     while (maxQuantity > 0):
+    #         cost = maxQuantity * a[i]
+    #         if k > cost:
+    #             k -= cost
+    #             stocks += maxQuantity
+    #             break
+    #         else:
+    #             maxQuantity -= 1
+    # return stocks
 
 if __name__ == "__main__":
     n = int(raw_input().strip())
