bcac79577af92efd5215e9d24a630915bc142c6e
Urian <urianchang@gmail.com>
2017-08-20 12:31:21 -0700

    Started working on Trader Profit algorithm

---
+++ HackerRank/GoldmanSachsCodeSprint/traderProfit.py
@@ -0,0 +1,32 @@
+"""
+Trader Profit:
+
+Mike is a stock trader and makes a profit by buying and selling stocks.
+He buys a stock at a lower price and sells it at a higher price to book
+a profit. He has come to know the stock prices of a particular stock for
+'n' upcoming days in future and wants to calculate the maximum profit
+by doing the right transactions (single transaction = buying + selling).
+Can you help him maximize his profit?
+
+NOTE: A transaction starts after the previous transaction has ended. Two
+transactions can't overlap or run in parallel.
+
+The stock prices are given in the form of an array A for n days.
+
+Given the stock prices and a positive integer k, find and print the maximum
+profit Mike can make in at most k transactions.
+"""
+
+import sys
+
+def traderProfit(k, n, A):
+    # Complete this function
+
+if __name__ == "__main__":
+    q = int(raw_input().strip())
+    for a0 in xrange(q):
+        k = int(raw_input().strip())
+        n = int(raw_input().strip())
+        arr = map(int, raw_input().strip().split(' '))
+        result = traderProfit(k, n, arr)
+        print result
