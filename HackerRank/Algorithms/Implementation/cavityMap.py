16bccb56dd8666b4b8267a98604732aa969e81f2
Urian <urianchang@gmail.com>
2017-10-04 22:13:04 -0700

    Completed Cavity Map algorithm

--- HackerRank/Algorithms/Implementation/cavityMap.py
+++ HackerRank/Algorithms/Implementation/cavityMap.py
@@ -15,8 +15,19 @@ n = int(raw_input().strip())
 grid = []
 grid_i = 0
 for grid_i in xrange(n):
-    grid_t = str(raw_input().strip())
+    grid_t = list(raw_input().strip())
     grid.append(grid_t)
+
+for i in xrange(1, n-1):
+    for j in xrange(1, n-1):
+        if (grid[i][j] > grid[i][j+1] and
+            grid[i][j] > grid[i][j-1] and
+            grid[i][j] > grid[i+1][j] and
+            grid[i][j] > grid[i-1][j]):
+            grid[i][j] = "X"
+
+for row in grid:
+    print ''.join(row)
 
 """
 Sample Input:
