c47dad22d788a1f089b99b1ebde1ee33c14fb703
Urian <urianchang@gmail.com>
2017-11-25 23:54:49 -0800

    Completed and optimized Sherlock and Array algorithm

--- HackerRank/Algorithms/Search/sherlockAndArray.py
+++ HackerRank/Algorithms/Search/sherlockAndArray.py
@@ -7,8 +7,24 @@ HackerRank url: https://www.hackerrank.com/challenges/sherlock-and-array/problem
 for _ in xrange(int(raw_input().strip())):
     n = int(raw_input().strip())    # number of elements
     A = map(int, raw_input().strip().split())   # Array
+    total = sum(A)
+    running_total = 0
     isBalanced = False
-    for i in xrange(n):
-        if sum(A[:i]) == sum(A[i+1:]):
+    for num in A:
+        if running_total == (total-(running_total+num)):
             isBalanced = True
+            break
+        running_total += num
     print "YES" if isBalanced else "NO"
+
+"""
+Input:
+3
+1 2 3
+4
+1 2 3 3
+
+Output:
+NO
+YES
+"""
