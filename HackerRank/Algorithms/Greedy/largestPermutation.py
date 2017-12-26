b650c08bca16c45e758eba31dff272177f4cc183
Urian <urianchang@gmail.com>
2017-12-25 21:11:41 -0800

    Attempt 2 only times out on TC 15

--- HackerRank/Algorithms/Greedy/largestPermutation.py
+++ HackerRank/Algorithms/Greedy/largestPermutation.py
@@ -10,13 +10,34 @@ perm = map(int, raw_input().strip().split())   # permutation of the first N natu
 s = 0
 p = 0
 
-while s < K and p < N:
-    mx = perm.index(max(perm[p:]))
-    if mx == p:
-        p += 1
-    else:
-        perm[p], perm[mx] = perm[mx], perm[p]
-        p += 1
-        s += 1
+# Attempt 1: Time out on TC 13 - 15
+# while s < K and p < N:
+#     mx = perm.index(max(perm[p:]))
+#     if mx == p:
+#         p += 1
+#     else:
+#         perm[p], perm[mx] = perm[mx], perm[p]
+#         p += 1
+#         s += 1
+#
+# print ' '.join(map(str, perm))
 
-print ' '.join(str(n) for n in perm)
+# Attempt 2: Timeout on TC 15
+d = {}
+for i in xrange(N):
+    d[perm[i]] = i
+
+if K < N:
+    while s < K and p < N:
+        mx = d[max(perm[p:])]
+        if mx == p:
+            p += 1
+        else:
+            perm[p], perm[mx] = perm[mx], perm[p]
+            d[perm[p]], d[perm[mx]] = d[perm[mx]], d[perm[p]]
+            p += 1
+            s += 1
+else:
+    perm.sort(reverse=True)
+
+print ' '.join(map(str, perm))
