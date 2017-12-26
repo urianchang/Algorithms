2095453220f99327b6e1ae3d18236f686081059c
Urian <urianchang@gmail.com>
2017-12-25 21:39:47 -0800

    Completed Largest Permutation algorithm

--- HackerRank/Algorithms/Greedy/largestPermutation.py
+++ HackerRank/Algorithms/Greedy/largestPermutation.py
@@ -7,10 +7,10 @@ URL: https://www.hackerrank.com/challenges/largest-permutation/problem
 N, K = map(int, raw_input().strip().split())   # size of list and maximum swaps
 perm = map(int, raw_input().strip().split())   # permutation of the first N natural numbers
 
-s = 0
 p = 0
 
 # Attempt 1: Time out on TC 13 - 15
+# s = 0
 # while s < K and p < N:
 #     mx = perm.index(max(perm[p:]))
 #     if mx == p:
@@ -23,20 +23,21 @@ p = 0
 # print ' '.join(map(str, perm))
 
 # Attempt 2: Timeout on TC 15
+# Attempt 3: Fix how index dictionary was used
 d = {}
 for i in xrange(N):
     d[perm[i]] = i
 
 if K < N:
-    while s < K and p < N:
-        mx = d[max(perm[p:])]
-        if mx == p:
+    while K > 0 and p < N:
+        if perm[p] == N - p:
             p += 1
         else:
-            perm[p], perm[mx] = perm[mx], perm[p]
-            d[perm[p]], d[perm[mx]] = d[perm[mx]], d[perm[p]]
+            max_idx = d[N - p]
+            d[N-p], d[perm[p]] = d[perm[p]], d[N-p]
+            perm[max_idx], perm[p] = perm[p], perm[max_idx]
             p += 1
-            s += 1
+            K -= 1
 else:
     perm.sort(reverse=True)
 
