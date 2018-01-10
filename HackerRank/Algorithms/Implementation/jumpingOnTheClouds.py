5131322909d12f3873f03c52a0ffd32d0afe82f6
Urian <urianchang@gmail.com>
2018-01-10 13:12:31 -0800

    Completed Jumping on the Clouds algorithm

--- HackerRank/Algorithms/Implementation/jumpingOnTheClouds.py
+++ HackerRank/Algorithms/Implementation/jumpingOnTheClouds.py
@@ -3,11 +3,20 @@ Jumping on the Clouds
 
 URL: https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem
 """
-
 import sys
 
 def jumpingOnClouds(c):
-    # Complete this function
+    i = 0
+    j = 0
+    while i < len(c) - 2:
+        if c[i+2] != 1:
+            i += 2
+        else:
+            i += 1
+        j += 1
+    if i == len(c) - 2:
+        j += 1
+    return j
 
 if __name__ == "__main__":
     n = int(raw_input().strip())
