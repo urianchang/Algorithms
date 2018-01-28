139a70fcde8a1c3799c46b32da6ce922b142aad4
Urian <urianchang@gmail.com>
2018-01-28 00:15:37 -0800

    Completed finding the percentage algo

--- HackerRank/Algorithms/Python/finding_the_percentage.py
+++ HackerRank/Algorithms/Python/finding_the_percentage.py
@@ -3,3 +3,8 @@ Finding the percentage
 
 URL: https://www.hackerrank.com/challenges/finding-the-percentage/problem
 """
+s = {}
+for _ in xrange(int(raw_input().strip())):
+    l = raw_input().strip().split()
+    s[l[0]] = "{:0.2f}".format((sum([float(i) for i in l[1:]]) / 3.0))
+print s[raw_input().strip()]
