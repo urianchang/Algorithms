91dad1c9818039bd4c5886566353a5f6a0a2d1eb
Urian <urianchang@gmail.com>
2018-02-04 23:32:09 -0800

    Completed Anagram algorithm

--- HackerRank/Algorithms/Strings/anagram.py
+++ HackerRank/Algorithms/Strings/anagram.py
@@ -7,10 +7,17 @@ URL: https://www.hackerrank.com/challenges/anagram/problem
 import sys
 
 def anagram(s):
-    # Complete this function
+    if len(s)%2 != 0:
+        return -1
+    s1 = list(s[:len(s)/2])
+    s2 = list(s[len(s)/2:])
+    for letter in s1:
+        if letter in s2:
+            s2.pop(s2.index(letter))
+    return len(s2)
 
-q = int(raw_input().strip())
+q = int(raw_input().strip())    # Number of TC's
 for a0 in xrange(q):
     s = raw_input().strip()
-    result = anagaram(s)
+    result = anagram(s)
     print(result)
