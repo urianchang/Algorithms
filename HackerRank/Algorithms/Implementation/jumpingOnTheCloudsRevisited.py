878f2a280434dc36cd5ccdd1ea9d73dc0b5b1657
Urian <urianchang@gmail.com>
2018-02-05 23:54:04 -0800

    Completed Jumping on Clouds Revisited

--- HackerRank/Algorithms/Implementation/jumpingOnTheCloudsRevisited.py
+++ HackerRank/Algorithms/Implementation/jumpingOnTheCloudsRevisited.py
@@ -6,10 +6,15 @@ URL: https://www.hackerrank.com/challenges/jumping-on-the-clouds-revisited/probl
 import sys
 
 def jumpingOnClouds(c, k):
-    # Complete this function
+    i = k%len(c)
+    e = 100 - (c[i] * 2 + 1)
+    while i != 0:
+        i = (i+k)%len(c)
+        e -= c[i] * 2 + 1
+    return e
 
 if __name__ == "__main__":
-    n, k = raw_input().strip().split(' ')
+    n, k = raw_input().strip().split(' ')   # Number of clouds and jump distance
     n, k = [int(n), int(k)]
     c = map(int, raw_input().strip().split(' '))
     result = jumpingOnClouds(c, k)
