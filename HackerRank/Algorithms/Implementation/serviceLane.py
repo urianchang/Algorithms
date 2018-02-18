2f4acd580b986710a7d2f7f8dc1a867ea4ae8035
Urian <urianchang@gmail.com>
2018-02-17 17:32:00 -0800

    Started working on Service Lane algorithm

---
+++ HackerRank/Algorithms/Implementation/serviceLane.py
@@ -0,0 +1,12 @@
+"""
+Service Lane
+
+URL: https://www.hackerrank.com/challenges/service-lane/problem
+"""
+
+N, T = map(int, raw_input().strip().split())
+width = map(int, raw_input().strip().split())
+
+for _ in xrange(T):
+    i, j = map(int, raw_input().strip().split())
+    
