5780a64e8671d05415dce1a9e47834cdf180b0c2
Urian <urianchang@gmail.com>
2018-02-18 22:41:10 -0800

    Started working on Compare Two Linked Lists

---
+++ HackerRank/Algorithms/DataStructures/LinkedLists/compareTwoLinkedLists.py
@@ -0,0 +1,21 @@
+"""
+Compare two linked lists
+
+URL: https://www.hackerrank.com/challenges/compare-two-linked-lists/problem
+"""
+
+"""
+ Compare two linked list
+ head could be None as well for empty list
+ Node is defined as
+
+ class Node(object):
+
+   def __init__(self, data=None, next_node=None):
+       self.data = data
+       self.next = next_node
+
+ return back the head of the linked list in the below method.
+"""
+
+def CompareLists(headA, headB):
