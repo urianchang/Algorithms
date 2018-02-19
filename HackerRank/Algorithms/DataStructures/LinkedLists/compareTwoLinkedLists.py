1500598ee547bb58bfc114c30baf242d047a3ce3
Urian <urianchang@gmail.com>
2018-02-18 23:12:27 -0800

    Completed Compare Two Linked Lists

--- HackerRank/Algorithms/DataStructures/LinkedLists/compareTwoLinkedLists.py
+++ HackerRank/Algorithms/DataStructures/LinkedLists/compareTwoLinkedLists.py
@@ -19,3 +19,11 @@ URL: https://www.hackerrank.com/challenges/compare-two-linked-lists/problem
 """
 
 def CompareLists(headA, headB):
+    r1 = headA
+    r2 = headB
+    while (r1 != None) and (r2 != None):
+        if r1.data != r2.data:
+            return 0
+        r1 = r1.next
+        r2 = r2.next
+    return int(getattr(r1, 'data', None) == getattr(r2, 'data', None))
