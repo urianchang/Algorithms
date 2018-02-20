148ccae4f5a8f89bb3565c91e5d6279ab695211d
Urian <urianchang@gmail.com>
2018-02-19 20:48:17 -0800

    Started working on Merge two linked lists algo

---
+++ HackerRank/Algorithms/DataStructures/LinkedLists/merge_two_sorted_linked_lists.py
@@ -0,0 +1,21 @@
+"""
+Merge Two Sorted Linked Lists
+
+URL: https://www.hackerrank.com/challenges/merge-two-sorted-linked-lists/problem
+"""
+
+"""
+ Merge two linked lists
+ head could be None as well for empty list
+ Node is defined as
+
+ class Node(object):
+
+   def __init__(self, data=None, next_node=None):
+       self.data = data
+       self.next = next_node
+
+ return back the head of the linked list in the below method.
+"""
+
+def MergeLists(headA, headB):
