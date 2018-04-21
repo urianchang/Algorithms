75a605acc35e022d3a577457bda5a5cb1c9501d3
Urian <urianchang@gmail.com>
2018-04-20 23:47:48 -0700

    Started working on Superior Characters algorithm

--- HackerRank/Week_Of_Code_37/superior_characters.py
+++ HackerRank/Week_Of_Code_37/superior_characters.py
@@ -3,3 +3,7 @@ Superior Characters
 
 URL: https://www.hackerrank.com/contests/w37/challenges/superior-characters
 """
+import math
+
+for _ in xrange(int(raw_input())):
+    frequencies = map(int, raw_input().strip().split())
