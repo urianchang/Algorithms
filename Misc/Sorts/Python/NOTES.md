46da3dbb89eedacbc58e04665c8735b2556e62cb
Urian <urianchang@gmail.com>
2018-06-08 20:50:37 -0700

    Added Insertion Sort with Python

--- Misc/Sorts/Python/NOTES.md
+++ Misc/Sorts/Python/NOTES.md
@@ -1,6 +1,14 @@
 # Sorting Algorithms
 
 ## Insertion sort
+Like sorting cards in your hand.
+
+__Time Complexity:__ O(n * n)
+__Space Complexity:__ O(1)
+__Boundary Cases:__ Maximum time to sort if elements are sorted in reverse order.
+Takes minimum time (O(n)) if elements are already sorted.
+__Uses:__ Used when number of elements is small. Can also be useful when input
+array is almost sorted.
 
 
 ## Selection sort
---
+++ Misc/Sorts/Python/insertion_sort.py
@@ -0,0 +1,38 @@
+# Python 2.7
+from datetime import datetime
+from contextlib import contextmanager
+
+
+@contextmanager
+def timer():
+    start = datetime.now()
+    yield
+    end = datetime.now()
+    print "Execution time: %s ms" % (end - start).microseconds
+
+
+def insertionSort(arr):
+    # type: (list) -> None
+    for i in range(1, len(arr)):
+        cur_val = arr[i]
+        pos = i-1
+
+        while pos >= 0 and cur_val < arr[pos]:
+            arr[pos + 1] = arr[pos]
+            pos -= 1
+
+        arr[pos + 1] = cur_val
+
+
+ex1 = [2, 1, 4, 5, 7, 1, 3, 9, 9, 2]
+ex2 = [9, 5, 6, 3, 2, 8, 1, 7, 10, 4]
+ex3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+ex4 = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+
+for ts in (ex1, ex2, ex3, ex4):
+    print "Before: %s" % ts
+
+    with timer():
+        insertionSort(ts)
+
+    print "After: %s\n" % ts
