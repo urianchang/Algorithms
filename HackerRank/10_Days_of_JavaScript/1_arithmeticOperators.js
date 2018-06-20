81f6bd54bbf2696c6836c56e06990ae42b9a02fd
Urian <urianchang@gmail.com>
2018-06-19 23:11:21 -0700

    Completed Day 1: Arithmetic Operators and let and const

---
+++ HackerRank/10_Days_of_JavaScript/1_arithmeticOperators.js
@@ -0,0 +1,31 @@
+//: https://www.hackerrank.com/challenges/js10-arithmetic-operators/problem
+
+/**
+*   Calculate the area of a rectangle.
+*
+*   length: The length of the rectangle.
+*   width: The width of the rectangle.
+*
+*	Return a number denoting the rectangle's area.
+**/
+function getArea(length, width) {
+    let area;
+    // Write your code here
+    area = length * width;
+    return area;
+}
+
+/**
+*   Calculate the perimeter of a rectangle.
+*
+*	length: The length of the rectangle.
+*   width: The width of the rectangle.
+*
+*	Return a number denoting the perimeter of a rectangle.
+**/
+function getPerimeter(length, width) {
+    let perimeter;
+    // Write your code here
+    perimeter = (length * 2) + (width * 2);
+    return perimeter;
+}
---
+++ HackerRank/10_Days_of_JavaScript/1_letAndConst.js
@@ -0,0 +1,6 @@
+//: https://www.hackerrank.com/challenges/js10-let-and-const/problem
+
+const PI = Math.PI;
+let r = Number(readLine());
+console.log(PI*r*r);
+console.log(2*PI*r);
