b82a75e73879ce99d0f495240aa658b02dcb3b19
Urian <urianchang@gmail.com>
2018-06-24 13:59:02 -0700

    Completed day 6: bitwise operator exercise

--- HackerRank/10_Days_of_JavaScript/6_bitwiseOperators.js
+++ HackerRank/10_Days_of_JavaScript/6_bitwiseOperators.js
@@ -1,1 +1,14 @@
 //: https://www.hackerrank.com/challenges/js10-bitwise/problem
+
+/*
+Additional Reading:
+  * https://www.hackerrank.com/challenges/js10-bitwise/topics/javascript-bitwise-operators
+  * https://www.hackerrank.com/topics/bit-manipulation
+*/
+
+function getMaxLessThanK(n, k) {
+    if ((k | k-1) > n) {
+        return (k-2);
+    }
+    return (k-1);
+}
