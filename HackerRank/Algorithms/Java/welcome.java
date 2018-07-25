aee64a45f9c1fc596a303cc8ee6f1a615a5c1f29
Urian <urianchang@gmail.com>
2018-07-24 21:17:59 -0700

    Added really basic hello world with Java

---
+++ HackerRank/Algorithms/Java/welcome.java
@@ -0,0 +1,9 @@
+//: https://www.hackerrank.com/challenges/welcome-to-java/problem
+
+public class Solution {
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
