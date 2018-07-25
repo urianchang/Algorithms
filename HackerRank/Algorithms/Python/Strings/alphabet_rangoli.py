894d1644b92ca2fc780fd0603e440282cc4ab368
Urian <urianchang@gmail.com>
2018-07-24 21:26:10 -0700

    Completed Alphabet Rangoli exercise

--- HackerRank/Algorithms/Python/Strings/alphabet_rangoli.py
+++ HackerRank/Algorithms/Python/Strings/alphabet_rangoli.py
@@ -1,4 +1,32 @@
 # https://www.hackerrank.com/challenges/alphabet-rangoli/problem
+import string
+
+
+ALPHA = string.ascii_lowercase
 
 def print_rangoli(size):
-    pass
+    for i in range(size - 1, 0, -1):
+        row = ["-"] * (size * 2 - 1)
+        for j in range(0, size - i):
+            row[size - 1 - j] = ALPHA[j + i]
+            row[size - 1 + j] = ALPHA[j + i]
+        print("-".join(row))
+
+    for i in range(0, size):
+        row = ["-"] * (size * 2 - 1)
+        for j in range(0, size - i):
+            row[size - 1 - j] = ALPHA[j + i]
+            row[size - 1 + j] = ALPHA[j + i]
+        print("-".join(row))
+
+print_rangoli(3)
+
+"""
+Expected:
+
+----c----
+--c-b-c--
+c-b-a-b-c
+--c-b-c--
+----c----
+"""
--- HackerRank/Algorithms/Python/alphabet_rangoli.py
+++
@@ -1,4 +0,0 @@
-# https://www.hackerrank.com/challenges/alphabet-rangoli/problem
-
-def print_rangoli(size):
-    pass
