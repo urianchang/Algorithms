fc2d08c76460bfa42225eb40886a88410bcbf43b
Urian <urianchang@gmail.com>
2018-08-09 00:33:40 -0700

    Completed integers exercise

---
+++ HackerRank/Algorithms/Python/Math/integers.py
@@ -0,0 +1,8 @@
+# https://www.hackerrank.com/challenges/python-integers-come-in-all-sizes/problem
+
+a = int(raw_input())
+b = int(raw_input())
+c = int(raw_input())
+d = int(raw_input())
+
+print pow(a, b) + pow(c, d)
