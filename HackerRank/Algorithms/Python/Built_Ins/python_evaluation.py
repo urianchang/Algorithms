9900d298eff1b449088843110ac247fba0703ed9
Urian <urianchang@gmail.com>
2018-10-06 20:18:51 -0700

    Completed Evaluation exercise

---
+++ HackerRank/Algorithms/Python/Built_Ins/python_evaluation.py
@@ -0,0 +1,12 @@
+# https://www.hackerrank.com/challenges/python-eval/problem
+
+"""
+`eval()`
+The `eval()` expression is a very powerful built-in function of Python. It helps
+in evaluating an expression. The expression can be a Python statement, or a code
+object.
+"""
+
+from __future__ import print_function
+
+eval(raw_input())
