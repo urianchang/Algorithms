5821b142843605c8af3fb456506c5dd648783b1f
Urian <urianchang@gmail.com>
2018-12-15 01:00:35 -0800

    Completed part 2 of day 12 of advent of code

--- AdventOfCode/2018/12_subterranean_sustainability/code.py
+++ AdventOfCode/2018/12_subterranean_sustainability/code.py
@@ -3,6 +3,44 @@ import copy
 from pprint import pprint
 
 
+def grow_once(plants, notebook):
+    # type: (list, dict) -> list
+    num_plants = len(plants)
+    new_plants = copy.copy(plants)
+    for p_idx in range(2, num_plants-2):
+        combo = ''.join(plants[p_idx-2:p_idx+3])
+        if combo in notes:
+            # print(p_idx, combo)
+            res = notes[combo]
+            new_plants[p_idx] = res
+        else:
+            new_plants[p_idx] = '.'
+    return new_plants
+
+
+def growth_for(num_years, tunnel, notes, debug=False):
+    # type: (int, list, dict) -> list
+    t = tunnel
+    growth_tracker = {0: count_plants(tunnel, length)}
+    for i in range(1, num_years+1):
+        t = grow_once(t, notes)
+        if debug:
+            num_plants = count_plants(t, length)
+            growth_tracker[i] = num_plants
+            diff = num_plants - growth_tracker[i-1]
+            print(f"Gen {i}: {diff}")
+    return t
+
+
+def count_plants(tunnel, section_length):
+    # type: (list, int) -> int
+    total = 0
+    for idx, pot in enumerate(tunnel):
+        if pot == "#":
+            total += idx - section_length
+    return total
+
+
 FILE = 'input.txt'
 initial_state = []
 notes = {}
@@ -18,40 +56,17 @@ with open(FILE, 'r') as fh:
                 combo, res = note.split(' => ')
                 notes[combo] = res
 
-# print(initial_state)
-# pprint(notes)
-width = len(initial_state)
-tunnel = (['.'] * width) + initial_state + (['.'] * width)
+length = len(initial_state) * 10
+tunnel = (['.'] * length) + initial_state + (['.'] * length)
+# tunnel_now = growth_for(20, tunnel, notes)
+# plants = count_plants(tunnel_now, length)
+# print(f"Part 1 - Number of plants @ gen20: {plants}")    # 1672
 
-def grow(plants, notebook):
-    # type: (list, dict) -> list
-    num_plants = len(plants)
-    new_plants = copy.copy(plants)
-    for p_idx in range(2, num_plants-2):
-        combo = ''.join(plants[p_idx-2:p_idx+3])
-        if combo in notes:
-            # print(p_idx, combo)
-            res = notes[combo]
-            new_plants[p_idx] = res
-        else:
-            new_plants[p_idx] = '.'
-    return new_plants
+# Part 2: Plants at generation 50_000_000_000 (50bn)
 
-
-# print(''.join(tunnel))
-
-# gen = {0: sum([1 for pot in tunnel if pot == "#"])}
-for i in range(1,21):
-    tunnel = grow(tunnel, notes)
-    # gen[i] = sum([1 for pot in tunnel if pot == "#"])
-
-# print(''.join(tunnel))
-# num = sum(gen.values())
-
-total = 0
-for idx, pot in enumerate(tunnel):
-    if pot == "#":
-        total += idx - width
-
-print(f"Part 1 - Number of plants: {total}")    # 1672
-
+# Is there a pattern?
+tunnel_now = growth_for(200, tunnel, notes, debug=False)
+_200 = count_plants(tunnel_now, length)
+# After generation 159, an additional 33 plants are added every year
+_50bn = (50_000_000_000 - 200) * 33 + _200
+print(f"Part 2 - Number of plants @ gen50bn: {_50bn}")  # 1650000000055
