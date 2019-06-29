b9acb2d8986bbee51142443b3e8a2c0360d2688b
Urian <urianchang@gmail.com>
2019-06-28 23:40:44 -0700

    Completed default arguments exercise

---
+++ HackerRank/Algorithms/Python/Debugging/default_arguments.py
@@ -0,0 +1,39 @@
+# Python 3
+# https://www.hackerrank.com/challenges/default-arguments/problem
+class EvenStream(object):
+    def __init__(self):
+        self.current = 0
+
+    def get_next(self):
+        to_return = self.current
+        self.current += 2
+        return to_return
+
+
+class OddStream(object):
+    def __init__(self):
+        self.current = 1
+
+    def get_next(self):
+        to_return = self.current
+        self.current += 2
+        return to_return
+
+
+# Debug this function
+def print_from_stream(n, stream=EvenStream()):
+    # Call constructor again
+    stream.__init__()
+    for _ in range(n):
+        print(stream.get_next())
+
+
+queries = int(input())
+for _ in range(queries):
+    stream_name, n = input().split()
+    n = int(n)
+    if stream_name == "even":
+        print_from_stream(n)
+    else:
+        print_from_stream(n, OddStream())
+
