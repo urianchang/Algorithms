5d2286dba019777c13483d648b18511db9a5b05e
Urian <urianchang@gmail.com>
2019-11-19 00:45:23 -0800

    Added examples of how uniqueness of set values are checked

--- Misc/JavaScript/sets_vs_arrays.js
+++ Misc/JavaScript/sets_vs_arrays.js
@@ -12,12 +12,21 @@ const mySet = new Set(myArr);
 console.log(mySet.values());
 
 // Adding values
+console.log("\n*** Adding values ***")
 mySet.add(4);
 mySet.add(2); // Value not added. No error thrown.
 console.log(mySet.values());
 
 // Removing values
+console.log("\n*** Removing values ***")
 mySet.delete(1);
 console.log(mySet.values());
 mySet.clear();
 console.log(mySet.values());
+
+// Sets use strict equality (===) to determine if value is unique. JavaScript
+// compares objects by reference (not contents).
+console.log("\n*** Uniqueness ***")
+console.log(new Set([1, 2, 3, "3"]).values());
+console.log(new Set([[1], [1]]).values());
+console.log(new Set([{a: 1}, {a: 1}]).values());
