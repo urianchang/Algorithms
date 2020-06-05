dc0a447500f07f335daf2e2128b9773986307d1f
Urian <urianchang@gmail.com>
2020-06-04 23:34:28 -0700

    Completed: Matrix script

--- HackerRank/Algorithms/Python/Regex_and_Parsing/matrix_script.py
+++ HackerRank/Algorithms/Python/Regex_and_Parsing/matrix_script.py
@@ -1,3 +1,28 @@
+# Python 3.7
 # https://www.hackerrank.com/challenges/matrix-script/problem
+import re
+from typing import List
 
 
+def decode_matrix(matrix: List[str], cols: int) -> str:
+    # Alphanumeric characters consist of: [A-Z, a-z, and 0-9]
+    decoded = ""
+    for i in range(cols):
+        for word in matrix:
+            decoded += word[i]
+
+    # Do lookbehind and lookahead
+    mre = re.compile('(?<=[A-Za-z0-9])[^A-Za-z0-9]+(?=[A-Za-z0-9])')
+    return mre.sub(" ", decoded)
+
+
+assert decode_matrix(["Tsi", "h%x", "i #", "sM ", "$a ", "#t%", "ir!"], 3) == "This is Matrix#  %!"
+
+
+n, m = map(lambda x: int(x), input().strip().split())
+
+matrix = []
+for _ in range(n):
+    matrix.append(input())
+
+print(decode_matrix(matrix, m))
