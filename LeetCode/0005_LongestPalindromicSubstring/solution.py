b4b1a2a1d12f0fa0752b046923c2c12203c00c87
Urian <urianchang@gmail.com>
2020-09-11 12:00:00 -0700

    WIP

---
+++ LeetCode/0005_LongestPalindromicSubstring/solution.py
@@ -0,0 +1,8 @@
+def longestPalindrome(s: str) -> str:
+    # Given a string s, find the longest palindromic substring in s.
+    # You may assume that the maximum length of s is 1000.
+    pass
+
+
+assert longestPalindrome("cbbd") == "bb"
+assert longestPalindrome("babad") in ("aba", "bab")
