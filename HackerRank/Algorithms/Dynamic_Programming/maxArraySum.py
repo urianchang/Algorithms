710539409cc0cde5bb2e618749fd11f84d9fc552
Urian <urianchang@gmail.com>
2020-09-22 12:00:00 -0700

    WIP

---
+++ HackerRank/Algorithms/Dynamic_Programming/maxArraySum.py
@@ -0,0 +1,11 @@
+# https://www.hackerrank.com/challenges/max-array-sum/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dynamic-programming
+
+
+def maxSubsetSum(arr):
+    # Find the subset of non-adjacent elements with the maximum sum
+    pass
+
+
+assert maxSubsetSum([3, 7, 4, 6, 5]) == 13
+assert maxSubsetSum([2, 1, 5, 8, 4]) == 11
+assert maxSubsetSum([3, 5, -7, 8, 10]) == 15
