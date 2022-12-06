79de49041c41c299ec6413e124a03a32e4a05bda
Urian Chang <urianchang@gmail.com>
2022-12-05 18:20:49 -0800

    Completed 'Unique Paths' exercise

--- LeetCode/0062_UniquePaths.py
+++ LeetCode/0062_UniquePaths.py
@@ -1,6 +1,17 @@
 # https://leetcode.com/problems/unique-paths/
+from math import factorial as fact
 
 
-class Solution:
-    def uniquePaths(self, m: int, n: int) -> int:
-        pass
+def uniquePaths(m: int, n: int) -> int:
+    # Subtract 1 from each position to account for origin and end positions
+    right = m - 1
+    down = n - 1
+
+    # Total moves that the robot has to take
+    moves = right + down
+
+    # Find combination of moves: moves!/(right! * down!)
+    return int(fact(moves)/(fact(right) * fact(down)))
+
+
+assert uniquePaths(3, 7) == 28
